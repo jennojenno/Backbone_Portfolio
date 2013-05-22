ac6e6c8d61334a441f35a84f2314983367f881c4
Jen Lim <jennolim@gmail.com>
2013-05-22 11:14:56 -0400

    Refactored index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require_tree .
+//= require jquery-1.9.1.min
+//= require underscore-min
+//= require backbone-min
+//= require backbone.localStorage-min
+//= require boot
+//= require router
+//= require models/project
+//= require models/user
+//= require collections/project_list
+//= require views/home
+//= require views/project_view
+//= require views/user_view
+//= require init
